cb32531573a7c80e392d8e30419c9a884876d03f
zhenyu92 <zchan012@e.ntu.edu.sg>
2019-06-26 23:22:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip3 install -r requirements.txt
 curl -o real_estate.csv  https://aisgaiap.blob.core.windows.net/aiap4-assessment/real_estate.csv
-python3 ./mlp/AIAP.py
+python3 AIAP.py
